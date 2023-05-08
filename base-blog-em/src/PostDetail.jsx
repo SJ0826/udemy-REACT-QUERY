e46386e68eae89f7deb908feb02723d0ab33ddf6
sujin <ikosdu60@gmail.com>
2023-05-08 19:39:41 +0900

    Feat: 페이지네이션에 프리패칭 작업 적용

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,9 +24,19 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
-
+  const { data, isError, error, isLoading } = useQuery(
+    // 문자열 comment에 식별자 역할을 하는 post.id 추가
+    ["comments", post.id],
+    () => fetchComments(post.id)
+  );
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
@@ -39,3 +51,17 @@ export function PostDetail({ post }) {
     </>
   );
 }
+
+// 첫번째 게시글을 클릭해서 나온 댓글이 다른 게시물을 클릭해도 업데이트되지 않은 이유
+// 쿼리 키 때문이다.
+// 모든 쿼리가 commnets라는 쿼리키를 동일하게 사용했기 때문이다. -> 어떤 트리거가 있어야만 데이터를 리페칭한다.
+// 트리거될 때
+// 1. 컴포넌트 리마운트 2. 윈도우 리포커징 3. 리패칭함수 실행 4. 자동 리패칭 5. mutation 이후 쿼리 무효화
+// 새 블로그 게시물 제목을 클릭해도 트리거가 발생하지 않았다. -> 따라서 데이터가 만료되어도 새 데이터를 가져오지 않는다.
+
+// 방법 1: 새 블로글 게시물 제목을 클릭할 때마다 데이터를 무효화시키기 -> 간단하지 않음. 데이터를 제거하는 것은 좋지 않은 방안.
+// 쿼리가 postID를 가지고 있기 때문에 쿼리 별로 데이터를 캐시할 수 있다.
+// 각 게시물에 대한 쿼리에 라벨을 설정한다! -> 쿼리키를 배열로 작성하면 가능하다!
+
+// 쿼리 키를 쿼리에 대한 의존성 배열로 취급한다.
+// 리액트 쿼리는 쿼리의 의존성 배열이 다르다면 완전히 다른 것으로 간주한다.
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,25 +1,42 @@
-import { useState } from "react";
-import { useQuery } from "react-query"; // 서버에서 데이터를 가져올 때 사용하는 훅
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query"; // 서버에서 데이터를 가져올 때 사용하는 훅
 
 import { PostDetail } from "./PostDetail";
 
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    // currentPage가 변경될 때마다 프리페칭 진행
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+
   // replace with useQuery
-  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, {
-    staleTime: 2000,
-  }); // 인자 1: 쿼리 키, 인자 2: 쿼리 함수 (비동기)
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+      keepPreviousData: true, // 쿼리키가 변경되어도, 새 데이터가 요청되는 동안 마지막으로 성공한 fetch data로 유지된다.
+    }
+  ); // 인자 1: 쿼리 키, 인자 2: 쿼리 함수 (비동기)
   if (isLoading) return <h3>Loading...</h3>;
   // 리액트 쿼리는 기본적으로 세번 로딩 시도 후 안되면 에러라고 판단.
   if (isError)
@@ -47,11 +64,22 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previosValue) => previosValue - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          // onClick함수에 프리페칭 작업을 하지 않는 이유: setState함수는 비동기로 실행되기 때문에 현재 페이지가 무엇인지 정확히 알 방법이 없다.
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
@@ -60,3 +88,12 @@ export function Posts() {
     </>
   );
 }
+
+// 리액트 쿼리를 이용해 프리페칭 하는 이유: 다음 페이지버튼을 눌렀을 때 로딩되고 있는 과정을 사용자가 경험하지 않고 바로 결과를 확인할 수 있게 하기 위해서. 데이터를 불러올 동안 사용자가 기다려야 한다.
+// 페이지가 캐시에 없어서 다음 버튼을 눌렀을 때 페이지가 로딩되길 기다려야 했다.
+
+// Prefetch?
+// 추후에 사용자가 사용할 데이터를 미리 가져온다.
+// 데이터를 미리 불러와 캐시에 데이터를 추가하여 저장하는 것
+// 기본값으로 만료 상태가 된다.
+// 캐시가 만료된 이후에 데이터를 다시 불러오면 로딩인디케이터가 나타나므로 캐시 타임을 적절히 설정해야 한다.
